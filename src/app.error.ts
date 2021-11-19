c3e8847bd53869dc5c0c557b16930aca76d3548c
Antanas Šepikas <antanas.sepikas@telesoftas.com>
2021-11-19 10:18:46 +0200

    link to swagger docs

--- src/app.error.ts
+++ src/app.error.ts
@@ -3,6 +3,9 @@ import { ApiProperty } from '@nestjs/swagger';
 export class AppErrorResource {
   @ApiProperty({
     type: 'object',
+    // I should be able to mark properties as required as in this example:
+    // https://swagger.io/docs/specification/data-models/data-types/#required
+    // required: ['name', 'status'],
     properties: {
       name: {
         type: 'string',
